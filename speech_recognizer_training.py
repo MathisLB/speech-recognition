d5ae2c6bbb3880b295d19003e77734f4d7526194
MathisLB <mathis.leboite@gmail.com>
2025-03-20 19:47:10 +0100

    Add speech recognizer training

---
+++ speech_recognizer_training.py
@@ -0,0 +1,7 @@
+import subprocess
+import os
+
+initial_dir = os.getcwd()
+os.chdir("speechbrain/templates/speech_recognition/ASR")
+subprocess.run(["python", "train.yaml", "--number_of_epochs=1", "--batch_size=2", "--enable_add_reverb=False", "--enable_add_noise=False"])
+os.chdir(initial_dir)
