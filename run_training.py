7ef2c94f85882b66af25a93d50da984cfe1c1d50
MathisLB <mathis.leboite@gmail.com>
2025-03-20 19:08:48 +0100

    Update training script to execute with CUDA support

--- run_training.py
+++ run_training.py
@@ -7,5 +7,5 @@ subprocess.run(["pip", "install", "datasets"])
 # Changer de répertoire
 os.chdir("/content/speechbrain/templates/speech_recognition/LM")
 
-# Exécuter le script d'entraînement
-subprocess.run(["python", "train.py", "RNNLM.yaml"])  # Ajoutez --device='cpu' si nécessaire
+# Exécuter le script d'entraînement avec CUDA
+subprocess.run(["python", "train.py", "RNNLM.yaml", "--device='cuda'"])
