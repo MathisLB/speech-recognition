b2c62b81573ca0bffee3921693651772ec29e318
MathisLB <mathis.leboite@gmail.com>
2025-03-20 19:10:29 +0100

    Remove unnecessary installation of datasets library in training script

--- run_training.py
+++ run_training.py
@@ -1,9 +1,6 @@
 import subprocess
 import os
 
-# Installer les bibliothèques nécessaires
-subprocess.run(["pip", "install", "datasets"])
-
 # Changer de répertoire
 os.chdir("/content/speechbrain/templates/speech_recognition/LM")
 
