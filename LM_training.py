b44d8207e982d2ddaebed2277c86922631d4714b
MathisLB <mathis.leboite@gmail.com>
2025-03-20 19:41:25 +0100

    Update directory paths in training scripts because not in jupyter

--- LM_training.py
+++ LM_training.py
@@ -2,6 +2,6 @@ import subprocess
 import os
 
 initial_dir = os.getcwd()
-os.chdir("/content/speechbrain/templates/speech_recognition/LM")
+os.chdir("speechbrain/templates/speech_recognition/LM")
 subprocess.run(["python", "train.py", "RNNLM.yaml", "--device=cuda"])
 os.chdir(initial_dir)
