16b29f31e417a0803deb0fe6c131fdeedc7011f1
MathisLB <mathis.leboite@gmail.com>
2025-03-20 19:57:27 +0100

    Refactor speech recognizer training script because of wrong command line

--- speech_recognizer_training.py
+++ speech_recognizer_training.py
@@ -3,5 +3,5 @@ import os
 
 initial_dir = os.getcwd()
 os.chdir("speechbrain/templates/speech_recognition/ASR")
-subprocess.run(["python", "train.yaml", "--number_of_epochs=1", "--batch_size=2", "--enable_add_reverb=False", "--enable_add_noise=False"])
+subprocess.run(["python", "train.py", "train.yaml", "--number_of_epochs=1", "--batch_size=2", "--enable_add_reverb=False", "--enable_add_noise=False"])
 os.chdir(initial_dir)
