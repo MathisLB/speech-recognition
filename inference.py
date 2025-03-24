d10e8908855cd24c1a286912bcd60d476150f17c
MathisLB <mathis.leboite@gmail.com>
2025-03-24 01:11:15 +0100

    Fix path to hyperparameters file in ASR inference script

--- inference.py
+++ inference.py
@@ -1,5 +1,5 @@
 from speechbrain.inference.ASR import EncoderDecoderASR
 
-asr_model = EncoderDecoderASR.from_hparams(source="results/CRDNN_BPE_960h_LM/2602/save", hparams_file='results/CRDNN_BPE_960h_LM/2602/hyperparams.yaml', savedir="modele_perso/")
+asr_model = EncoderDecoderASR.from_hparams(source="results/CRDNN_BPE_960h_LM/2602/save", hparams_file="../hyperparams.yaml", savedir="modele_perso/")
 audio_file = 'test.wav'
 asr_model.transcribe_file(audio_file)
