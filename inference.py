44c0f017473f4e6fd13433839d860e850cfebae3
MathisLB <mathis.leboite@gmail.com>
2025-03-24 23:57:58 +0100

    Add ASR and language model checkpoints, update inference script, and modify hyperparameters

--- inference.py
+++ inference.py
@@ -1,5 +1,6 @@
 from speechbrain.inference.ASR import EncoderDecoderASR
 
 asr_model = EncoderDecoderASR.from_hparams(source="results/CRDNN_BPE_960h_LM/2602/save", hparams_file="../hyperparams.yaml", savedir="modele_perso/")
-audio_file = 'test.wav'
-asr_model.transcribe_file(audio_file)
+audio_file = '2412-153948-0004.wav'
+transcription = asr_model.transcribe_file(audio_file)
+print("Transcription :", transcription)
